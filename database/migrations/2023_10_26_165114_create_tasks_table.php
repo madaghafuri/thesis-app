61d7214adb835dfdff951d7410b24ee5c0bb18f1
madaghafuri <muhammadghafuri6301@gmail.com>
2023-11-06 23:24:05 +0700

    feat: add priority seeder and model relation

--- database/migrations/2023_10_26_165114_create_tasks_table.php
+++ database/migrations/2023_10_26_165114_create_tasks_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function PHPUnit\Framework\once;
+
 return new class extends Migration
 {
     /**
@@ -16,9 +18,11 @@ return new class extends Migration
             $table->bigInteger('user_id')->nullable()->unsigned();
             $table->bigInteger('section_id')->unsigned();
             $table->bigInteger('project_id')->unsigned();
+            $table->bigInteger('priority_id')->nullable()->unsigned();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('section_id')->references('id')->on('sections')->onDelete('cascade');
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->foreign('priority_id')->references('id')->on('priorities')->onDelete('cascade');
             $table->string('name')->nullable();
             $table->date('due_date')->nullable();
             $table->date('start_date')->nullable();
