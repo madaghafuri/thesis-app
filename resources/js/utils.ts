aa802cb39d942f59ec63e890b2af7e15d17d1f90
madaghafuri <muhammadghafuri6301@gmail.com>
2023-11-06 23:24:28 +0700

    feat: add isObjectEmpty utils

--- resources/js/utils.ts
+++ resources/js/utils.ts
@@ -5,3 +5,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function isObjectEmpty(objName: Object) {
+    return Object.keys(objName).length === 0;
+}
