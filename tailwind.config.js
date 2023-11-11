e6a481680cedd0ee140dc55cfe5241ba92f7f6a9
madaghafuri <muhammadghafuri6301@gmail.com>
2023-11-11 13:36:23 +0700

    feat: add accordion component

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,20 @@ export default {
                 indigo: '#8d84e8',
                 magenta: '#f9aaef',
                 pinl: '#ffc7db'
+            },
+            keyframes: {
+                "accordion-down": {
+                    form: { height: "0" },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)"},
+                    to: { height: "0" },
+                }
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
             }
         },
     },
