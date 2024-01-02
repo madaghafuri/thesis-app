31dc29d133794dcd48ebacf5f412edb4f5e371f6
madaghafuri <muhammadghafuri6301@gmail.com>
2024-01-02 17:29:24 +0700

    fix: accumulated working hours based on date

--- resources/js/Components/Workspace/Project/Workload/hooks.tsx
+++ resources/js/Components/Workspace/Project/Workload/hooks.tsx
@@ -1,5 +1,13 @@
 import { Task, WorkloadUser } from "@/types";
-import { differenceInDays, getDay, secondsToHours } from "date-fns";
+import {
+    differenceInDays,
+    eachDayOfInterval,
+    endOfWeek,
+    formatISO,
+    getDay,
+    secondsToHours,
+    startOfWeek,
+} from "date-fns";
 import { useEffect, useRef, useState } from "react";
 
 export function useDayCellWidth() {
@@ -10,9 +18,9 @@ export function useDayCellWidth() {
         if (dayRef.current !== null) {
             setDayWidth(dayRef.current.clientWidth);
         }
-    }, [])
+    }, []);
 
-    return { dayWidth, setDayWidth, dayRef }
+    return { dayWidth, setDayWidth, dayRef };
 }
 /**
  * a react hook to calculate the size of a EventBlock components
@@ -20,26 +28,64 @@ export function useDayCellWidth() {
  * @param endDate due date of a task in Date format
  * @param dayCellWidth width an day Element Header
  */
-export function useTaskSize(startDate: Date, endDate: Date, dayCellWidth: number) {
+export function useTaskSize(
+    startDate: Date,
+    endDate: Date,
+    dayCellWidth: number
+) {
     const dayDiff = differenceInDays(endDate, startDate);
     const weekDayStart = getDay(startDate);
     // Absolute left position in percentage string ex: "100%"
-    const leftPos = Math.abs((weekDayStart / 7) * 100).toFixed(3).toString();
-    const dayDifferences = dayDiff > (7 - weekDayStart) ? (7 - weekDayStart) : dayDiff;
+    const leftPos = Math.abs((weekDayStart / 7) * 100)
+        .toFixed(3)
+        .toString();
+    const dayDifferences =
+        dayDiff > 7 - weekDayStart ? 7 - weekDayStart : dayDiff;
     // Size of a task width relative to startDate and endDate in pixel(px)
-    const rangeDurationWidth = Math.abs(dayCellWidth * dayDifferences).toString() + "px";
+    const rangeDurationWidth =
+        Math.abs(dayCellWidth * dayDifferences).toString() + "px";
 
     return {
         leftPos,
-        rangeDurationWidth
-    }
+        rangeDurationWidth,
+    };
 }
-export function getAccumulatedHoursByUser(user: WorkloadUser) {
-    const accumulatedTimeForUser = user.tasks.map((val) => {
-        const accumulatedTaskTime = val.times.reduce((time, curr) => time += curr.duration, 0);
-        return accumulatedTaskTime
-    });
+export function getAccumulatedHoursByUser(user: WorkloadUser, date: Date) {
+    // const accumulatedTimeForUser = user.tasks.map((val) => {
+    //     const accumulatedTaskTime = val.times.reduce(
+    //         (time, curr) => (time += curr.duration),
+    //         0
+    //     );
+    //     return accumulatedTaskTime;
+    // });
 
-    const seconds = accumulatedTimeForUser.reduce((taskTime, curr) => taskTime += curr, 0);
+    const currentDate = date;
+    const start = startOfWeek(currentDate);
+    const end = endOfWeek(currentDate);
+    const eachDayInterval = eachDayOfInterval({ start, end }).map((val) =>
+        formatISO(val, {
+            representation: "date",
+        })
+    );
+
+    const accumulatedTimeForUser = user.tasks
+        .filter((value) => {
+            return (
+                eachDayInterval.includes(value.start_date) ||
+                eachDayInterval.includes(value.due_date)
+            );
+        })
+        .map((val) => {
+            const accumulatedTaskTime = val.times.reduce(
+                (time, curr) => (time += curr.duration),
+                0
+            );
+            return accumulatedTaskTime;
+        });
+
+    const seconds = accumulatedTimeForUser.reduce(
+        (taskTime, curr) => (taskTime += curr),
+        0
+    );
     return secondsToHours(seconds);
 }
